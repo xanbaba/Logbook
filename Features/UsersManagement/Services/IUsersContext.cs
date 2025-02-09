a85eae52023622a0ec0689cf6e267969e84440e0
XanbabaF <xanbabaf@gmail.com>
2025-02-09 21:29:03 +0400

    Defining interface for managing users

---
+++ Features/UsersManagement/Services/IUsersContext.cs
@@ -0,0 +1,23 @@
+﻿using Logbook.Entities;
+
+namespace Logbook.Features.UsersManagement.Services;
+
+public interface IUsersContext
+{
+    // Get
+    public Task<IEnumerable<User>> GetUsersAsync();
+    public Task<IEnumerable<User>> GetUsersByRoleAsync(UserRole role);
+    public Task<User?> GetUserByIdAsync(Guid id);
+    public Task<User?> GetUserByLoginAsync(string login);
+    public Task<User?> GetUserByEmailAsync(string email);
+    
+    // Add
+    public Task<User> AddUserAsync(User user);
+    
+    // Delete
+    public Task<User> DeleteUserAsync(User user);
+    public Task<User> DeleteUserAsync(Guid id);
+    
+    // Update
+    public Task<User> UpdateUserAsync(User user);
+}
