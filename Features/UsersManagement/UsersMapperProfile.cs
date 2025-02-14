ccbf6cc4a8d1b99c5c2fef2009b67c58abbcb236
XanbabaF <xanbabaf@gmail.com>
2025-02-14 23:09:05 +0400

    configuring auto mapper

---
+++ Features/UsersManagement/UsersMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Logbook.Entities;
+
+namespace Logbook.Features.UsersManagement;
+
+public class UsersMapperProfile : Profile
+{
+    public UsersMapperProfile()
+    {
+        CreateMap<User, UserDTO>().ReverseMap();
+    }
+}
