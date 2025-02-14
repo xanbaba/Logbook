b92f41aedb001c6666b309df4ef57536a1839bff
XanbabaF <xanbabaf@gmail.com>
2025-02-14 23:10:20 +0400

    Defining endpoints. Some are left

--- Features/UsersManagement/UsersManagementEndpointMapper.cs
+++ Features/UsersManagement/UsersManagementEndpointMapper.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics.CodeAnalysis;
+using AutoMapper;
+using FluentValidation;
+using Logbook.Entities;
+using Logbook.Features.UsersManagement.Exceptions;
+using Logbook.Features.UsersManagement.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Logbook.Features.UsersManagement;
 
@@ -12,6 +19,12 @@ public abstract class UsersManagementEndpointMapper : IEndpointMapper
         // GET /users/0194e5af-ae13-714b-a974-1acee1f19cd3
         source.MapGet("/users/{id:guid}", GetUser);
 
+        // GET /users/by-email/ooo0o@gmail.com
+        source.MapGet("/users/by-email/{email}", (string email) => { });
+
+        // GET /users/by-login/xanbaba
+        source.MapGet("/users/by-login/{login}", (string login) => { });
+
         // POST /users
         source.MapPost("/users", CreateUser);
 
@@ -20,30 +33,140 @@ public abstract class UsersManagementEndpointMapper : IEndpointMapper
 
         // DELETE /users/0194e5af-ae13-714b-a974-1acee1f19cd3
         source.MapDelete("/users/{id:guid}", DeleteUser);
+
+        source.MapGet("/users/students", () => { });
+        source.MapGet("/users/teachers", () => { });
+        source.MapGet("/users/admins", () => { });
     }
 
-    private static Task<IResult> GetUser(Guid id)
+    [SuppressMessage("ReSharper", "NotAccessedPositionalProperty.Local")]
+    private record GetUserResponse(UserDTO User, Guid Id);
+
+    private static async Task<Results<Ok<GetUserResponse>, NotFound>> GetUser
+    (
+        [FromRoute] Guid id,
+        [FromServices] IMapper mapper,
+        [FromServices] IUsersContext usersContext
+    )
     {
-        throw new NotImplementedException();
+        var user = await usersContext.GetUserByIdAsync(id);
+        if (user == null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        return TypedResults.Ok(new GetUserResponse(mapper.Map<User, UserDTO>(user), id));
     }
 
-    private static Task<IResult> GetUsers(int offset = 0, int count = 100)
+    private static async Task<Ok<UserDTO[]>> GetUsers
+    (
+        [FromServices] IUsersContext usersContext,
+        [FromServices] IMapper mapper,
+        [AsParameters] int offset,
+        [AsParameters] int count
+    )
     {
-        throw new NotImplementedException();
+        if (offset <= 0) offset = 0;
+
+        if (count <= 0) count = 30;
+        else if (count > 100) count = 100;
+
+        var usersRaw = await usersContext.GetUsersAsync();
+        var users = usersRaw.Skip(offset).Take(count).Select(mapper.Map<User, UserDTO>)
+            .ToArray();
+
+        return TypedResults.Ok(users);
     }
 
-    private static Task<IResult> CreateUser([FromBody] UserDTO? dto)
+    private static async Task<Results<Created, BadRequest<string>, Conflict<string>>> CreateUser
+    (
+        [FromServices] IUsersContext usersContext,
+        [FromServices] IMapper mapper,
+        [FromServices] IValidator<UserDTO> validator,
+        [FromBody] UserDTO? dto
+    )
     {
-        throw new NotImplementedException();
+        if (dto is null)
+        {
+            return TypedResults.BadRequest("Provide user data");
+        }
+
+        var validationResult = await validator.ValidateAsync(dto);
+        if (!validationResult.IsValid)
+        {
+            return TypedResults.BadRequest(validationResult.ToString("\n"));
+        }
+
+        var mappedUser = mapper.Map<UserDTO, User>(dto);
+        
+        var hashedPassword = PasswordHasher.HashPassword(dto.Password!);
+        mappedUser.PasswordHash = hashedPassword;
+        
+        try
+        {
+            var user = await usersContext.AddUserAsync(mappedUser);
+
+            return TypedResults.Created($"users/{user.Id}");
+        }
+        catch (UniqueConstraintException e)
+        {
+            return TypedResults.Conflict(e.Message);
+        }
     }
 
-    private static Task<IResult> UpdateUser(Guid id, [FromBody] UserDTO? dto)
+    private static async Task<Results<BadRequest<string>, NotFound<string>, Ok, Conflict<string>>> UpdateUser
+    (
+        [FromRoute] Guid id,
+        [FromBody] UserDTO? dto,
+        [FromServices] IUsersContext usersContext,
+        [FromServices] IMapper mapper,
+        [FromServices] IValidator<UserDTO> validator
+    )
     {
-        throw new NotImplementedException();
+        if (dto is null)
+        {
+            return TypedResults.BadRequest("Provide user data");
+        }
+
+        var validationResult = await validator.ValidateAsync(dto);
+        if (!validationResult.IsValid)
+        {
+            return TypedResults.BadRequest(validationResult.ToString("\n"));
+        }
+
+        var mappedUser = mapper.Map<UserDTO, User>(dto);
+        mappedUser.PasswordHash = PasswordHasher.HashPassword(dto.Password!);
+
+        try
+        {
+            await usersContext.UpdateUserAsync(mappedUser);
+            return TypedResults.Ok();
+        }
+        catch (UniqueConstraintException e)
+        {
+            return TypedResults.Conflict(e.Message);
+        }
+        catch (UserNotFoundException e)
+        {
+            return TypedResults.NotFound(e.Message);
+        }
     }
 
-    private static Task<IResult> DeleteUser(Guid id)
+    private static async Task<Results<NotFound<string>, Ok>> DeleteUser
+    (
+        [FromRoute] Guid id,
+        [FromServices] IUsersContext usersContext,
+        [FromServices] IMapper mapper
+    )
     {
-        throw new NotImplementedException();
+        try
+        {
+            await usersContext.DeleteUserAsync(id);
+            return TypedResults.Ok();
+        }
+        catch (UserNotFoundException e)
+        {
+            return TypedResults.NotFound(e.Message);
+        }
     }
 }
