38a5b61d22c1abe82bdccb86f74f9e64e1f1b540
XanbabaF <xanbabaf@gmail.com>
2025-02-14 23:05:21 +0400

    Password Hasher

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+﻿using System.Text;
+
+namespace Logbook;
+
+public static class Constants
+{
+    public static Encoding DefaultEncoding { get; set; } = Encoding.UTF8;
+}
---
+++ Features/UsersManagement/Services/PasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace Logbook.Features.UsersManagement.Services;
+
+public static class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        var bytes = Constants.DefaultEncoding.GetBytes(password);
+        return Convert.ToBase64String(bytes);
+    }
+}
