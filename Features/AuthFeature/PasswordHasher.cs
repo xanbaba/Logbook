cbaa4cace0d9e8cc4f63b8667d6e07259ac8d374
XanbabaF <xanbabaf@gmail.com>
2025-02-18 20:39:49 +0400

    adding SHA256 hashing algorithm to PasswordHasher.cs

---
+++ Features/AuthFeature/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+namespace Logbook.Features.AuthFeature;
+
+public static class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        var passwordBytes = Constants.DefaultEncoding.GetBytes(password);
+        var hashedPasswordBytes = SHA256.HashData(passwordBytes);
+        var hashedPassword = Convert.ToBase64String(hashedPasswordBytes);
+        return hashedPassword;
+    }
+}
