cb09e7733c9d9c50a40af996baabb7e556cd7014
XanbabaF <xanbabaf@gmail.com>
2025-02-18 21:32:13 +0400

    configuring Authentication and policy based Authorization

--- Features/AuthFeature/AuthFeature.cs
+++ Features/AuthFeature/AuthFeature.cs
@@ -1,4 +1,8 @@
-﻿namespace Logbook.Features.AuthFeature;
+﻿using Logbook.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Logbook.Features.AuthFeature;
 
 public abstract class AuthFeature : IFeature
 {
@@ -6,10 +10,39 @@ public abstract class AuthFeature : IFeature
     {
         builder.Services.AddScoped<JwtTokenGenerator>();
         builder.Services.AddScoped<IRefreshTokensManager, RefreshTokensManager>();
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = builder.Configuration.GetJwtIssuer(),
+
+                ValidateAudience = true,
+                ValidAudience = builder.Configuration.GetJwtAudience(),
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(Constants.DefaultEncoding.GetBytes(builder.Configuration.GetJwtSecret())),
+
+                ValidateLifetime = true
+            };
+        });
+
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy(UserRole.Admin.ToString(), policyBuilder =>
+            {
+                policyBuilder.RequireRole(UserRole.Admin.ToString());
+            });
+        });
     }
 
     public static void Configure(WebApplication app)
     {
         app.MapGroup("/api/v1").MapEndpoints<AuthEndpointMapper>();
+        
+        app.UseAuthentication();
+        app.UseAuthorization();
     }
 }
